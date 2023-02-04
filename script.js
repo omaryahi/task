da4374d6e989fe3211ae9843d739ae8ed5ab0d19
omaryahi <94490017+omaryahi@users.noreply.github.com>
2023-02-04 11:48:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let a,b,c
+document.getElementById("bt").onclick=function(){
+    a=document.getElementById("a").value
+    b=document.getElementById("b").value
+    c=document.getElementById("c").value
+    if ((Math.pow(b, 2)-(4*a*c))>0) {
+        console.log(((-b+Math.sqrt(Math.pow(b ,2)-(4*a*c))))/(2*a))
+        console.log(((+b-Math.sqrt(Math.pow(b ,2)-(4*a*c))))/(2*a))
+
+    }
+    else if ((Math.pow(b, 2)-(4*a*c))==0) {
+       
+        console.log((-b+Math.sqrt((Math.pow(b,2))))/2*a)
+
+    }
+    else{
+        console.log("no solution")
+
+    }
+
+}
